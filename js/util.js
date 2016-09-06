f69254faab780351b9f2c30e5366eff23be38705
Devis Lee <598252892@qq.com>
2016-09-07 01:04:15 +0800

    ddd

---
+++ js/util.js
@@ -0,0 +1,24 @@
+ /**
+  *  LOG
+  */
+ function log(str){
+ 	console.log(str);
+ }
+ 
+function snackbar(text){
+	snackbar(text, 2000);
+}
+
+function snackbar(text, duration){
+	$(document.body).snackbar({
+		alive: duration,
+		content: text
+	});
+}
+ 
+ 
+//database
+//添加key-value 数据到 sessionStorage
+//      localStorage.setItem("demokey", "http://blog.itjeek.com");
+//      //通过key来获取value
+//      var dt = localStorage.getItem("demokey");
