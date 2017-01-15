3cd96e743b7f6b31bfe46d9d520e3b6e6f6eb078
Devis Lee <598252892@qq.com>
2017-01-15 20:33:00 +0800

    = =

--- js/request.js
+++ js/request.js
@@ -1,34 +1,37 @@
 var appId = 'MPPk3pQdp6kIl6tAJI2HWuDF';
 var appKey = 'TsuMnwADjcIkD1po2kKn8U7F';
 
-AV.initialize(appId, appKey);
+AV.init({
+  appId: appId,
+  appKey: appKey
+});
    // 实例化统计分析功能
-var analytics = AV.analytics({
-    // 设置 AppId
-    appId: appId,
-    // 设置 AppKey
-    appKey: appKey,
-    // 你当前应用或者想要指定的版本号（自定义）
-    version: '4.0.0',
-    // 你当前应用的渠道或者你想指定的渠道（自定义）
-    channel: 'develop'
-});
+//var analytics = AV.analytics({
+//  // 设置 AppId
+//  appId: appId,
+//  // 设置 AppKey
+//  appKey: appKey,
+//  // 你当前应用或者想要指定的版本号（自定义）
+//  version: '4.0.0',
+//  // 你当前应用的渠道或者你想指定的渠道（自定义）
+//  channel: 'develop'
+//});
 
-analytics.send({
-    // 事件名称
-    event: 'view_in',
-//
-//  // 事件属性，任意数据
-//  attr: {
-//      testa: 123,
-//      testb: 'abc'
-//  },
-    // 该事件持续时间（毫秒）
-    duration: 6000}, function(result) {
-    if (result) {
-        console.log('成功统计到用户！');
-    }
-});
+//analytics.send({
+//  // 事件名称
+//  event: 'view_in',
+////
+////  // 事件属性，任意数据
+////  attr: {
+////      testa: 123,
+////      testb: 'abc'
+////  },
+//  // 该事件持续时间（毫秒）
+//  duration: 6000}, function(result) {
+//  if (result) {
+//      console.log('成功统计到用户！');
+//  }
+//});
 
 function updateCatHit(objectId){
 	var Post = AV.Object.extend('Cat');
