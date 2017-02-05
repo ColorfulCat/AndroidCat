1e4d9d2753f10fb07adc35a2d23f5fd5512f3def
Devis Lee <598252892@qq.com>
2017-02-06 00:24:00 +0800

    ssss

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
 //var header = new mdui.Headroom('#header');
 //var mTab = new mdui.Tab('#tab');
 var drawer = new mdui.Drawer('#main-drawer');
-
 var searchIndex = 0;
 var currentSelection = 0;
 var lastId = -1;
 var idOffset = 0;
-var containerLayout = $("");
+var containerLayout = $("#mdui-container");
 var catMenusDiv = $("#bookmarkMenuLayout");
+
 var searchLayout = $("#searchLayout")
 var listLayout = $("#listLayout");
+var bookmarkLayout = $("#bookmarkLayout");
+var searchInput = $("#searchInput");
 var mCats = [];
+
+var searchGoogle = "https://www.google.com.hk/webhp?#safe=strict&q=";
+var searchBaidu = "https://www.baidu.com/s?wd=";
+var searchGithub = "https://github.com/search?q=";
+var searchUrl = searchGoogle;
 
 var needRefresh = true;
 
@@ -31,9 +38,12 @@ $(document).ready(function() {
 	}
 
 	initSideMenu();
-//	queryCats("AndroidCat", "recommend");
+	//	queryCats("AndroidCat", "recommend");
 	updateMenus(-1)
 	initWeather();
+	show(searchLayout);
+	hide(bookmarkLayout);
+
 });
 
 function initWeather() {
@@ -109,12 +119,12 @@ function createBookmarkMenu(title, tag) {
 			queryCats(title, tag);
 			updateMenus(currentId);
 			log("screen.width = " + document.documentElement.clientWidth);
-			if(document.documentElement.clientWidth <= 800){
-				if(drawer){
+			if(document.documentElement.clientWidth <= 800) {
+				if(drawer) {
 					log("close1")
 					drawer.close();
 					log("close2")
-				}else{
+				} else {
 					log("open")
 				}
 			}
@@ -136,15 +146,15 @@ function updateMenus(id) {
 		$("#menuId" + lastId).removeClass("mdui-list-item-active"); //移除样式
 		$("#aboutMenu").removeClass("mdui-list-item-active"); //移除样式
 		$("#homepageMenu").removeClass("mdui-list-item-active"); //移除样式
-		searchLayout.hide();//移除搜索
-		listLayout.show();//展示列表
+		hide(searchLayout);
+		show(bookmarkLayout);
 		log("click list")
 	} else {
 		if(id == -1) { // 首页
 			$("#homepageMenu").addClass("mdui-list-item-active"); //添加样式
 			$("#aboutMenu").removeClass("mdui-list-item-active"); //移除样式
-			searchLayout.show();//移除搜索
-			listLayout.hide();//展示列表
+			show(searchLayout);
+			hide(bookmarkLayout);
 			log("click -1")
 		} else if(id == -2) { //关于
 			$("#aboutMenu").addClass("mdui-list-item-active"); //添加样式
@@ -158,7 +168,7 @@ function updateMenus(id) {
 
 function createBookmarks(item) {
 	var divP = $('<div class="mdui-col-sm-12 mdui-col-md-6 mdui-col-lg-4"></div>');
-	var liP = $('<li class="mdui-list-item mdui-ripple bookMarkItem"></li>');
+	var liP = $('<li class="mdui-list-item mdui-ripple bookMarkItem" rel ="noopener"></li>');
 	var imgC = $('<img class="avatar" src="' + item.icon + '" />')
 	var divC = $('<div class="content">' + item.title + '</div>')
 	divP.append(liP);
@@ -173,7 +183,7 @@ function createBookmarks(item) {
 }
 
 function clearCatItem() {
-	listLayout.fadeOut(300);
+	//	listLayout.fadeOut(300);
 }
 
 function queryCats(title, tag) {
@@ -181,21 +191,21 @@ function queryCats(title, tag) {
 	clearCatItem();
 
 	$("#listTitle").text(title);
-//	if(needRefresh) {
-		queryResults(tag);
-//	} else {
-//		var dataTemp = localStorage.getItem("tag_" + tag);
-//		var dataObj = null;
-//		if(dataTemp != null){
-//			dataObj = JSON.parse(dataTemp);
-//		}
-//		
-//		if(dataObj != null) {
-//			setResults(dataObj.data);
-//		} else {
-//			queryResults(tag);
-//		}
-//	}
+	//	if(needRefresh) {
+	queryResults(tag);
+	//	} else {
+	//		var dataTemp = localStorage.getItem("tag_" + tag);
+	//		var dataObj = null;
+	//		if(dataTemp != null){
+	//			dataObj = JSON.parse(dataTemp);
+	//		}
+	//		
+	//		if(dataObj != null) {
+	//			setResults(dataObj.data);
+	//		} else {
+	//			queryResults(tag);
+	//		}
+	//	}
 
 }
 
@@ -208,10 +218,10 @@ function queryResults(tag) {
 	}
 	query.addDescending('grade');
 	query.find().then(function(results) {
-//		var jsonData = {};
-//		jsonData.data = JSON.stringify(results);
-//		log("jsondata = " + jsonData)
-//		localStorage.setItem("tag_" + tag, JSON.stringify(jsonData));
+		//		var jsonData = {};
+		//		jsonData.data = JSON.stringify(results);
+		//		log("jsondata = " + jsonData)
+		//		localStorage.setItem("tag_" + tag, JSON.stringify(jsonData));
 		setResults(results);
 	}, function(error) {
 		log('Error: ' + error.code + ' ' + error.message);
@@ -255,22 +265,51 @@ function setResults(results) {
 			'scrollTop': 0
 		}, 500);
 		//展示
-		listLayout.fadeIn(300);
+		//		listLayout.fadeIn(300);
 	} else {
 		log("000");
 	}
 }
 
-function refresh(){
+function refresh() {
 	localStorage.setItem("city", "");
 	localStorage.setItem("weatherString", "");
 	localStorage.setItem("lastRefreshTime", 0);
 	window.location.reload();
 }
 
-function selectSearch(index){
-	if(searchIndex != index){
+function selectSearch(index) {
+	if(searchIndex != index) {
 		searchIndex = index;
 		log("selectSearch index = " + index);
+		switch(index) {
+			case 0:
+				searchUrl = searchGoogle;
+				break;
+			case 1:
+				searchUrl = searchBaidu;
+				break;
+			case 2:
+				searchUrl = searchGithub;
+				break;
+			default:
+				searchUrl = searchBaidu;
+		}
 	}
+}
+
+function search() {
+	setTimeout(function() {
+		window.open(searchUrl + document.getElementById("searchInput").value, '', '');
+	}, 200);
+}
+
+function hide(obj) {
+	obj.addClass("mdui-hidden");
+	//	obj.fadeOut(300);
+}
+
+function show(obj) {
+	obj.removeClass("mdui-hidden");
+	//	obj.fadeIn(300);
 }
