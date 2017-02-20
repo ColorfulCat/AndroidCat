e5559247828c7541fb346c1f13b61549cc13935d
封羽 <devislee.lb@alibaba-inc.com>
2017-02-20 20:35:50 +0800

    r

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,10 +2,6 @@
 	<div class="about">
 		<div class="title">这是关于页面</div>
 		<h5>{{msg}}</h5>
-    <div id="editor">
-      <textarea :value="input" @input="update"></textarea>
-      <div v-html="compiledMarkdown"></div>
-    </div>
 	</div>
 </template>
 
@@ -14,20 +10,9 @@
 		name: 'about',
 		data() {
 			return {
-				msg: '啦啦啦',
-        input: '# hello'
+				msg: '啦啦啦'
 			}
-		},
-    computed: {
-      compiledMarkdown: function () {
-        return marked(this.input, { sanitize: true })
-      }
-    },
-    methods: {
-      update: _.debounce(function (e) {
-        this.input = e.target.value
-      }, 300)
-    }
+		}
 	}
 </script>
 <style scoped>
@@ -41,35 +26,4 @@
 		margin: 16px;
 		text-align: center;
 	}
-
-  html, body, #editor {
-    margin: 0;
-    height: 100%;
-    font-family: 'Helvetica Neue', Arial, sans-serif;
-    color: #333;
-  }
-
-  textarea, #editor div {
-    display: inline-block;
-    width: 49%;
-    height: 100%;
-    vertical-align: top;
-    box-sizing: border-box;
-    padding: 0 20px;
-  }
-
-  textarea {
-    border: none;
-    border-right: 1px solid #ccc;
-    resize: none;
-    outline: none;
-    background-color: #f6f6f6;
-    font-size: 14px;
-    font-family: 'Monaco', courier, monospace;
-    padding: 20px;
-  }
-
-  code {
-    color: #f66;
-  }
 </style>
