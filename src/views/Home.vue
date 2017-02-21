5248c003c275906cfba652dd16f6ea421e6a1ded
Devis Lee <598252892@qq.com>
2017-02-22 01:10:12 +0800

    grid

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,101 +1,86 @@
 <template>
-	<div class="home">
-		<div class="title">这是首页</div>
-		<h5>{{msg}}</h5>
-		<p>111111</p>
-		<div>
-			<mu-flexbox>
-				<mu-flexbox-item class="flex-demo">
+  <div class="home">
+    <div class="title">这是首页</div>
+    <h5>Android  ╮(╯▽╰)╭  </h5>
+    <div class="contentLayout">
+      <mu-row gutter>
+        <mu-col width="20" tablet="20" desktop="20">
+          <div id="markMenuLayout">
+            <CatMenuItem :cat_menu_item_title="menu"></CatMenuItem>
+            <CatMenuItem :cat_menu_item_title="menu"></CatMenuItem>
+            <CatMenuItem :cat_menu_item_title="menu"></CatMenuItem>
+            <CatMenuItem :cat_menu_item_title="menu"></CatMenuItem>
+            <CatMenuItem :cat_menu_item_title="menu"></CatMenuItem>
+          </div>
+        </mu-col>
+        <mu-col width="80" tablet="80" desktop="80">
+          <mu-row gutter id="markListLayout">
+            <mu-col width="100" tablet="50" desktop="33">
+              <CatItem :cat_item_title="item"></CatItem>
+            </mu-col>
+            <mu-col width="100" tablet="50" desktop="33">
+              <CatItem :cat_item_title="item"></CatItem>
+            </mu-col>
+            <mu-col width="100" tablet="50" desktop="33">
+              <CatItem :cat_item_title="item"></CatItem>
+            </mu-col>
+            <mu-col width="100" tablet="50" desktop="33">
+              <CatItem :cat_item_title="item"></CatItem>
+            </mu-col>
+            <mu-col width="100" tablet="50" desktop="33">
+              <CatItem :cat_item_title="item"></CatItem>
+            </mu-col>
+            <mu-col width="100" tablet="50" desktop="33">
+              <CatItem :cat_item_title="item"></CatItem>
+            </mu-col>
 
-				</mu-flexbox-item>
-				<mu-flexbox-item class="flex-demo">
-					2
-				</mu-flexbox-item>
-				<mu-flexbox-item class="flex-demo">
-					3
-				</mu-flexbox-item>
-			</mu-flexbox>
-			<mu-flexbox class="mt8">
-				<mu-flexbox-item class="flex-demo">
-					4
-				</mu-flexbox-item>
-				<mu-flexbox-item class="flex-demo">
-					5
-				</mu-flexbox-item>
-			</mu-flexbox>
-			<mu-flexbox class="mt8">
-				<mu-flexbox-item order="0" class="flex-demo">
-					6
-				</mu-flexbox-item>
-				<mu-flexbox-item order="2" span="200" class="flex-demo">
-					7
-				</mu-flexbox-item>
-				<mu-flexbox-item order="1" class="flex-demo">
-					8
-				</mu-flexbox-item>
-			</mu-flexbox>
-			<mu-flexbox class="mt8" orient="vertical">
-				<mu-flexbox-item order="0" class="flex-demo">
-          <CatItem :cat_item_title="msg"></CatItem>
-				</mu-flexbox-item>
-				<mu-flexbox-item order="2" class="flex-demo">
-					10
-				</mu-flexbox-item>
-        <mu-flexbox-item order="2" class="flex-demo">
-          10
-        </mu-flexbox-item>
-        <mu-flexbox-item order="2" class="flex-demo">
-          10
-        </mu-flexbox-item>
-        <mu-flexbox-item order="2" class="flex-demo">
-          10
-        </mu-flexbox-item>
-        <mu-flexbox-item order="2" class="flex-demo">
-          10
-        </mu-flexbox-item>
-        <mu-flexbox-item order="2" class="flex-demo">
-          10
-        </mu-flexbox-item>
-			</mu-flexbox>
-		</div>
-
-	</div>
+          </mu-row>
+        </mu-col>
+      </mu-row>
+    </div>
+  </div>
 </template>
 
 <script>
+  import CatMenuItem from 'components/CatMenuItem'
   import CatItem from 'components/CatItem'
-	export default {
-		name: 'home',
-		data() {
-			return {
-				msg: 'Welcome to Your Vue.js App'
-			}
-		},
+  export default {
+    name: 'home',
+    data() {
+      return {
+        menu: '精品推荐',
+        item: '稀土掘金'
+      }
+    },
     components: {
-      CatItem
+      CatItem, CatMenuItem
     }
-	}
+  }
 </script>
 <style scoped>
-	h5 {
-		text-align: center;
-		font-size: 2rem;
-	}
+  h5 {
+    text-align: center;
+    font-size: 2rem;
+  }
 
-	.title {
-		font-size: 3rem;
-		margin: 16px;
-		text-align: center;
-	}
+  .title {
+    font-size: 3rem;
+    margin: 16px;
+    text-align: center;
+  }
 
-	.mt8 {
-		margin-top: 8px;
-	}
+  .mt8 {
+    margin-top: 8px;
+  }
 
-	.flex-demo {
-		height: 48px;
-		background-color: #e0e0e0;
-		text-align: center;
-		line-height: 48px;
-	}
+  .flex-demo {
+    height: 48px;
+    background-color: #e0e0e0;
+    text-align: center;
+    line-height: 48px;
+  }
+
+  .contentLayout {
+    text-align: center;
+  }
 </style>
